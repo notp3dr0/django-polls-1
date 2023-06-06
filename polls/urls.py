a5e6ea8040e6ad34794f34ca861194724f7c7ba9
Pedro augusto <guto.siqueiralino@gmail.com>
2023-06-06 11:22:49 -0300

    terminal atualizado

--- polls/urls.py
+++ polls/urls.py
@@ -6,6 +6,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('sobre/', views.sobre, name='sobre')
+    path('pergunta/<int:question_id>', views.exibe_questao, name= 'exibe_questao')
 ]
 
 # .\venv\Scripts\activate.bat
